75b0e15a41c2696250c92637862b6c0b3c9fc5bf
Blake La Pierre <blakelapierre@gmail.com>
2015-08-05 03:36:21 -0700

    Hopefully this will work on the first try.

---
+++ frontend/publish.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ `git rev-parse --abbrev-ref HEAD` != "develop" ]; then
+  echo "Not on develop"
+  exit 1
+fi
+
+test -z "$(git status --porcelain)"
+if [ $? != 0 ]; then
+  echo "Repo not clean"
+  exit 1
+fi
+
+gulpur build \
+  && REV=`git rev-parse HEAD` \
+  && cd ../../2016-fantasy.github.io \
+`#  && git rm -r *` \
+  && cp -r -v ../www/frontend/.dist/* ./ \
+  && git add . --all \
+  && git commit -m "Publishing $REV" \
+  && git push origin master \
+  && echo "Published!" \
+|| echo "Something failed. Figure it out!"
