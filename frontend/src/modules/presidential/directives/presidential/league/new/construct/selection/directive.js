c6c3d6da85e13470a7ec19f23a17568f6c56f4d4
Blake La Pierre <blakelapierre@gmail.com>
2015-08-25 23:02:24 -0700

    Working on this

--- frontend/src/modules/presidential/directives/presidential/league/new/construct/selection/directive.js
+++ frontend/src/modules/presidential/directives/presidential/league/new/construct/selection/directive.js
@@ -50,3 +50,101 @@ function state(s) {
 
   };
 }
+
+const states = {
+  'candidate-tier': ['candidate-tier'],
+  'stable': ['stable', 'candidate-tier'],
+  'set-draft': ['set-draft', 'stable'],
+  'invite': ['set-draft', 'invite', 'stable'],
+  'pre-game': ['pre-game', 'stable'],
+  'countdown': ['countdown', 'stable'],
+  'draft': ['stable', 'draft']
+};
+const a = `
+states {
+  candidate-tier:
+     : [candidate-tier]
+    -> stable
+  stable:
+     : [stable, candidate-tier]
+    -> candidate-tier
+    -> set-draft
+  set-draft:
+     : [set-draft, stable]
+    -> stable
+    -> invite
+  invite:
+     : [invite, stable]
+    -> set-draft
+    -> pre-game
+  pre-game:
+     : [pre-game, stable]
+    -> countdown
+    -> draft
+  countdown:
+     : [countdown, stable]
+    -> draft
+  draft:
+     : [stable, draft]
+    -> game
+  game:
+     : [game]
+}`;
+
+const b = parse(a);
+
+console.log({b});
+
+function parse(string) { // a terrible parser
+  const lines = string.split('\n'),
+        states = {};
+
+  let state;
+
+  for (let i = 2; i < lines.length - 1; i++) { // skip the first two and last lines
+    const line = lines[i].trim(),
+          {length} = line;
+
+    if (line.lastIndexOf(':') === length - 1) { // new state
+      const [name] = line.split(':');
+
+      state = states[name];
+
+      if (state) throw Error(`Duplicate state ${name}`);
+
+      state = states[name] = {
+        values: [],
+        transitions: []
+      };
+    }
+    else if (line.indexOf(':') === 0) { // state property
+      const list = line.split(':')[1],
+            values = /\[(.*)\]/g.exec(list)[1].split(',').map(value => value.trim());
+
+      state.values.push(...values);
+    }
+    else if (line.indexOf('->') === 0) { // transition
+      const transition = line.split('->')[1].trim();
+
+      state.transitions.push(transition);
+    }
+
+  }
+
+  return states;
+}
+
+// states = {
+//   'candidate-tier': {
+//     values: ['candidate-tier'],
+//     transitions: ['stable']
+//   },
+//   'stable': {
+//     values: ['stable', 'candidate-tier'],
+//     transitions: ['candidate-tier', 'set-draft']
+//   },
+//   'set-draft': {
+//     values: ['set-draft', 'stable'],
+//     transitions: ['stable', 'invite']
+//   }
+// };
