e0ba4b8dbd941381205d9371c98f52a2f071f449
Blake La Pierre <blakelapierre@gmail.com>
2015-09-01 00:47:38 -0700

    A work in progress

--- frontend/src/modules/presidential/directives/presidential/directive.js
+++ frontend/src/modules/presidential/directives/presidential/directive.js
@@ -3,7 +3,131 @@ export default () => {
     restrict: 'E',
     template: require('./template.html'),
     controller: ['$scope', $scope => {
-
+      // This was tool-generated elsewhere
+      $scope.stageGraph = {
+         "presidential-frontend": {
+            "name": "presidential-frontend",
+            "states": {
+               "teaser": {
+                  "properties": [
+                     "teaser"
+                  ],
+                  "transitions": [
+                     "intro"
+                  ]
+               },
+               "intro": {
+                  "properties": [
+                     "intro"
+                  ],
+                  "transitions": [
+                     "choose"
+                  ]
+               },
+               "choose": {
+                  "properties": [
+                     "choose"
+                  ],
+                  "transitions": [
+                     "new-league",
+                     "existing-league"
+                  ]
+               }
+            }
+         },
+         "new-league": {
+            "name": "new-league",
+            "start": "candidate-tier",
+            "states": {
+               "candidate-tier": {
+                  "properties": [
+                     "candidate-tier"
+                  ],
+                  "transitions": [
+                     "stable"
+                  ]
+               },
+               "stable": {
+                  "properties": [
+                     "stable",
+                     "candidate-tier"
+                  ],
+                  "transitions": [
+                     "candidate-tier",
+                     "set-draft"
+                  ]
+               },
+               "set-draft": {
+                  "properties": [
+                     "set-draft",
+                     "stable"
+                  ],
+                  "transitions": [
+                     "stable",
+                     "invite"
+                  ]
+               },
+               "invite": {
+                  "properties": [
+                     "invite",
+                     "stable"
+                  ],
+                  "transitions": [
+                     "set-draft",
+                     "pre-game"
+                  ]
+               },
+               "pre-game": {
+                  "properties": [
+                     "pre-game",
+                     "stable"
+                  ],
+                  "transitions": [
+                     "countdown",
+                     "draft"
+                  ]
+               },
+               "countdown": {
+                  "properties": [
+                     "countdown",
+                     "stable"
+                  ],
+                  "transitions": [
+                     "draft"
+                  ]
+               },
+               "draft": {
+                  "properties": [
+                     "stable",
+                     "draft"
+                  ],
+                  "transitions": [
+                     "game"
+                  ]
+               },
+               "game": {
+                  "properties": [
+                     "game"
+                  ],
+                  "transitions": []
+               }
+            }
+         },
+         "existing-league": {
+            "name": "existing-league",
+            "states": {
+               "selector": {
+                  "properties": [
+                     "selector"
+                  ],
+                  "transitions": [
+                     "choose",
+                     "random"
+                  ]
+               }
+            }
+         }
+      }['new-league'];
     }]
   };
 };
--- frontend/src/modules/state-graph/directives/stage/directive.js
+++ frontend/src/modules/state-graph/directives/stage/directive.js
@@ -1,8 +1,113 @@
+import _ from 'lodash';
+import HashList from 'hash-list';
+
 export default ['$compile', ($compile) => {
   return {
     restrict: 'E',
+    scope: {
+      'graph': '='
+    },
     link: ($scope, element, attributes) => {
-      element.parent().append($compile('<div>wut wut</div>')($scope));
+      $scope.visible = {};
+
+      const current = new HashList();
+
+      let currentState;
+
+      $scope.$watch('graph', ({states, start}) => setState(states[start]));
+
+      // element.parent().append($compile('<div>wut wut</div>')($scope));
+      // setStage();
+
+      // function setStage() {
+      //   console.log({$scope});
+      //   const {graph, visible} = $scope,
+      //         {properties} = graph,
+      //         track = _.transform(properties, ({name}) => false),
+      //         {length} = properties;
+
+      //   let position = 0;
+
+      //   for (let i = 0; i < length; i++) {
+      //     const p = properties[position],
+      //           index = current.indexOf(p);
+
+      //     console.log({i, index, p});
+
+      //     if (index === -1) {
+      //       visible[p] = true;
+      //       current.insertAt(p, position);
+      //       element.append($compile(`<${p} ng-if="visible['${p}']"></${p}>`)($scope));
+      //     }
+      //     else {
+      //       visible[p] = false;
+      //     }
+
+      //     position++;
+
+      //     track[p] = true;
+      //   }
+
+      //   // _.each(_.where(track, value => !value), toRemove => {
+      //   //   current.removeById(toRemove);
+      //   // });
+      //   _.each(_.where(track, value => !value), current.removeById);
+      // }
+
+      function transitionTo(state) {
+        setState(getNewState(state));
+
+        function getNewState(state) {
+          const {graph} = $scope,
+                {transitions} = currentState,
+                transition = transitions[state];
+
+          if (!transition) throw Error(`Tried to transition to non-existent state ${state}`);
+
+          return graph.states[transition];
+        }
+      }
+
+      function setState(state) {
+        const {visible} = $scope,
+              {properties} = state,
+              {length} = properties,
+              track = toMap(properties);
+
+        console.log('setState', state, properties, track, length);
+        let position = 0;
+
+        for (let i = 0; i < length; i++) {
+          const p = properties[position],
+                index = current.indexOf(p);
+
+          console.log({i, index, p});
+
+          if (index === -1) {
+            visible[p] = true;
+            current.insertAt(p, position);
+            element.append($compile(`<${p} ng-if="visible['${p}']"></${p}>`)($scope));
+          }
+          else {
+            visible[p] = false;
+          }
+
+          position++;
+
+          track[p] = true;
+        }
+
+        // _.each(_.where(track, value => !value), toRemove => {
+        //   current.removeById(toRemove);
+        // });
+        _.each(_.where(track, value => !value), current.removeById);
+
+        currentState = state;
+      }
+
+      function toMap(array) {
+        return _.transform(array, (result, value) => result[value] = true, {});
+      }
     },
     controller: ['$scope', $scope => {
 
